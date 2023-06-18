4e9ded04524d04fa95a50b8f191a377c5b0c178d
Felipe0206 <conocimiento.02@gmail.com>
2023-06-18 17:51:04 -0500

    Actualizacionn

--- ap_Softseguros/settings.py
+++ ap_Softseguros/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(j1=fd8hdc^a8m#b8$-o7hs0zc@_o^5$b20zzfug96+k=9s1sq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
